2019700cc78a336fbedb6f4041fe0e798ae61fce
bogdanbele <bogdan.bele@yahoo.com>
2018-05-21 16:25:12 +0200

    reading json from files

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const path    = require("path");
 const fs = require('fs');
 const glob = require('glob');
 
-let bundler = new Bundler('src/pages/home/index.pug');
 const app = express();
-app.set(express.static(__dirname + '/dist'));
-
-
-
 
 let sitePath = __dirname+ `/dist/*/index.html`;
 console.log(sitePath);
 const siteFiles = glob.sync(sitePath);
 console.log(siteFiles);
 
+
+
+
+
 siteFiles.forEach(file => {
   const requestPath = file.split('dist/')[1];
   const expressPath = '/'+requestPath.split('/')[0];
   console.log(expressPath);
+
+
+
+  const data = fs.readFileSync(__dirname+'/src/pages'+expressPath+'/data.json', 'utf8');
+  console.log(data);
+
+
 
   app.get(expressPath,function(req,res){
     res.sendFile(path.join(__dirname+'/dist/'+requestPath));
@@ -27,6 +33,19 @@ siteFiles.forEach(file => {
   });
 
 });
+
+
+let bundler = new Bundler('src/pages/home/index.pug');
+
+app.set(express.static(__dirname + '/dist'));
+
+
+
+
+
+
+
+
 app.use(bundler.middleware());
 
 
