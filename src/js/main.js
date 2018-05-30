3a0ddc2a995374209874fa8f3b7a911aa34305ed
bogdanbele <bogdan.bele@yahoo.com>
2018-05-30 09:22:36 +0200

    Working header links

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,17 @@ import message from "./message";
 message('Hello World!');
 
 console.log('meme00');
+
+$(function() {
+    const pathName = location.pathname
+    const substring = pathName.substring(pathName.lastIndexOf('/')+1);
+    const locationPath = location.pathname.split("/")[1];
+    console.log(pathName);
+    console.log(substring);
+    console.log();
+    if ( locationPath !== ''){
+    $('.links a[href="' + pathName + '"]').addClass('active');
+    }else{
+        $('#home').addClass('active');
+    }
+});
