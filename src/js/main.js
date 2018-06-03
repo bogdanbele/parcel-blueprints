ab85c22b9b2353baa3c1c89f882e2a35fc3f3651
bogdanbele <bogdan.bele@yahoo.com>
2018-06-03 15:38:50 +0200

    Show Active parent page

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,14 @@ console.log('meme00');
 
 $(function() {
     const pathName = location.pathname
-    const substring = pathName.substring(pathName.lastIndexOf('/')+1);
-    const locationPath = location.pathname.split("/")[1];
+    const substring = pathName.substring(pathName.indexOf('/',1));
+    const locationPath = '/'+location.pathname.split("/")[1];
     console.log(pathName);
     console.log(substring);
-    console.log();
+    console.log(locationPath);
     if ( locationPath !== ''){
-    $('.links a[href="' + pathName + '"]').parent().addClass('active');
+    
+    $('.links a[href="' + locationPath + '"]').parent().addClass('active');
     }else{
         $('#home').addClass('active');
     }
